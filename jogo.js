88e2a0df2fc38449167b286204ade211d00d2089
andrearaujo1706 <57424258+andrearaujo1706@users.noreply.github.com>
2019-11-20 23:56:23 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -215,12 +215,29 @@ background(200);
     moverVilao();
   }
 
-if(x==xv1+20 && y<=465||x==xv2+20 && y<=465||x==xv3+20 && y<=465 ){
+if(x==xv1+20 && y<=465){
+  vida=vida-1
+}else{
+if(x==xv1-20 && y<=465){
   vida=vida-1
 }
-if(x==xv1-20 && y<=465||x==xv2-20 && y<=465||x==xv3-20 && y<=465 ){
+}
+if(x==xv2+20 && y<=465){
+  vida=vida-1
+}else{
+if(x==xv2-20 && y<=465){
   vida=vida-1
 }
+}
+if(x==xv3+20 && y<=465){
+  vida=vida-1
+}else{
+if(x==xv3-20 && y<=465){
+  vida=vida-1
+}
+}
+
+
 
 function vidas(){
     if(vida==3){
